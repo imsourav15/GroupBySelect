0e4af1cc037ac755c5ae1a36b0a88faf0d5d922f
imsourav15 <sourav.shandilya@juspay.in>
2023-10-09 00:47:13 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Select from "react-select";
 import './App.css';
 
-function App() {
+
+// Adding Different Groups of doctor's let's suppose seegerated on the basis of Area, State or Locality.
+const group1Options = [
+  {
+    label: "Dr. Varun Sharma",
+    value: "Noida Sector 52"
+  },
+  {
+    label: "Dr. Kartik Modi",
+    value: "Hauz Khas"
+  },
+  {
+    label: "Dr. Shruti Gupta",
+    value: "Greater Kailash"
+  }
+];
+
+const group2Options = [
+  {
+    label: "Dr. Sourav Suman",
+    value: "Gurgaon"
+  },
+  {
+    label: "Dr. Ravi Bishnoi",
+    value: "Ghitorni"
+  },
+  {
+    label: "Dr. Kritika Sharma",
+    value: "Iffco Chowk"
+  }
+];
+
+// The Function which is used to create doctor's group, basis on whatever  criteriaon's.
+
+const createDoctorsGroup = (groupName, options) => {
+  return {
+    label: groupName,
+    options: options
+  };
+};
+
+export default function App() {
+
+    // Using React Hook's USeStae which allows us to have state variables in functional components.
+  const [value, setValue] = useState([]);
+  const options = [
+    createDoctorsGroup("---DOCTOR's GROUP 1---", group1Options),
+    createDoctorsGroup("---DOCTOR's GROUP 2---", group2Options)
+  ];
+
+
+  // The feature flag should be called groupSelectAll
+
+  const groupSelectAll = (groupOptions) => {
+    setValue((selectedOptions) => [
+      ...selectedOptions,
+      ...groupOptions.filter(
+        (grpOpt) => !selectedOptions.some((opt) => opt.value === grpOpt.value)
+      )
+    ]);
+  };
+
+
+// The Return function where class APP is getting called.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bonjour Doctor's!</h1>
+      <Select
+        onChange={(selectedOptions) => {
+          console.log(selectedOptions);
+          setValue(selectedOptions);
+        }}
+        // This is made false when you select it doesn't closes by default.
+        closeMenuOnSelect={false}
+        isMulti
+        menuIsOpen={true}
+        options={options}
+        value={value}
+        components={{
+          GroupHeading: ({ innerProps, children }) => (
+            <div
+              {...innerProps}
+              onClick={() => groupSelectAll(children.props.options)}
+            >
+              {children}
+            </div>
+          )
+        }}
+      />
     </div>
   );
 }
-
-export default App;
